e5a87db70eaa31ea92fca6e0edab5acb59d0dee2
ninja4panda <ninja4panda@gmail.com>
2020-07-12 19:38:20 +1000

    Start implementing game class

--- server/Game.js
+++ server/Game.js
@@ -1,20 +1,37 @@
 const crypto = require("crypto");
 const gameRooms = [];
 
+//3 channels players, mafia, spectator
 module.exports = class Game {
-    //field 
-    room;
-    numPlayers = 1;
-
+    //private field 
+    #players = [];
+    #mafia = [];
+    #police;
+    #witch;
+    #started = true;
 
     createGame(data, socket) {
-        console.log(data.name);
-        gameRooms.push(crypto.randomBytes(4).toString('hex'));
+        //check in case the same room already exists
+        var room = crypto.randomBytes(4).toString('hex');
+        while(gameRooms.includes(room)) room = crypto.randomBytes(4).toString('hex');
+        gameRooms.push(room);
+        //put client to the room
+        socket.join(room)
         console.log(gameRooms);
     }
 
     //static funciton for joining game
     static joinGame({name, room}, socket) {
         console.log(name);
+        if(started) ;
+    }
+
+    //Start Game
+    startGame() {
+
+    }
+    //assign roles
+    assignRoles() {
+
     }
 }
