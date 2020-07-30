3dda8e5b627e3a928658ebf19510c2eab5825374
Ninja4Panda <ninja4panda@gmail.com>
2020-07-30 10:42:01 +1000

    added error page handling

--- index.js
+++ index.js
@@ -5,33 +5,40 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const Game = require('./server/Game');
 
-//this builds all the static js files for reacts
-app.use(express.static(path.join(__dirname+'/client/build')));
+//Builds all public files for pages
+app.use(express.static(path.join(__dirname+'/public')));
+app.use(express.static(path.join(__dirname+'/public/killerPanda')));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/client/build/index.html');
+//=========== routes ==============
+app.get('/game',(req, res) => {
+  res.sendFile(__dirname+'/public/killerPanda/game.html');
+  
+  //on every client connection
+  io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+    
+    // listen to when a client creates a new room
+    socket.on('Create Game', (data) => {
+      const game = new Game();
+      game.createGame(data, socket);
+    });
+    
+    // listen to when a client wants to join a room
+    socket.on('Join Game', (data) => {
+      Game.joinGame(data, socket);
+    });
+    
+    // listen to when a client disconnect
+    socket.on('disconnect', () => {
+      console.log('disconnecting:', socket.id);
+    });
+  });
 });
 
-//on every client connection
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-
-  // listen to when a client creates a new room
-  socket.on('Create Game', (data) => {
-    const game = new Game();
-    game.createGame(data, socket);
-  });
-
-  // listen to when a client wants to join a room
-  socket.on('Join Game', (data) => {
-    Game.joinGame(data, socket);
-  });
-
-  socket.on('disconnect', () =>{
-    console.log('disconnecting:', socket.id);
-  });
+// Handles page not found error
+app.use((req, res) => {
+  res.status(404).send("OH NO YOU DIED >_<");
 });
-
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
