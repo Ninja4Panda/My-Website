dc85c5157c244f524ad2202403cc78ae91f5360c
Ninja4Panda <ninja4panda@gmail.com>
2020-08-01 09:37:07 +1000

    socket-io working again

--- index.js
+++ index.js
@@ -9,30 +9,31 @@ const Game = require('./server/Game');
 app.use(express.static(path.join(__dirname+'/public')));
 app.use(express.static(path.join(__dirname+'/public/killerPanda')));
 
+//on every client connection
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  
+  // listen to when a client creates a new room
+  socket.on('Create Game', (data) => {
+    const game = new Game();
+    game.createGame(data, socket);
+  });
+  
+  // listen to when a client wants to join a room
+  socket.on('Join Game', (data) => {
+    Game.joinGame(data, socket);
+  });
+  
+  // listen to when a client disconnect
+  socket.on('disconnect', () => {
+    console.log('disconnecting:', socket.id);
+  });
+});
+
 //=========== routes ==============
 app.get('/game',(req, res) => {
   res.sendFile(__dirname+'/public/killerPanda/game.html');
   
-  //on every client connection
-  io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-    
-    // listen to when a client creates a new room
-    socket.on('Create Game', (data) => {
-      const game = new Game();
-      game.createGame(data, socket);
-    });
-    
-    // listen to when a client wants to join a room
-    socket.on('Join Game', (data) => {
-      Game.joinGame(data, socket);
-    });
-    
-    // listen to when a client disconnect
-    socket.on('disconnect', () => {
-      console.log('disconnecting:', socket.id);
-    });
-  });
 });
 
 // Handles page not found error
