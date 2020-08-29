96c3429508bbe6ed1ff606d5483ae9e5829ec81c
Ninja4Panda <ninja4panda@gmail.com>
2020-08-29 11:00:04 +1000

    restructed io to a seperate file

--- index.js
+++ index.js
@@ -2,45 +2,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);//set options & use cors/csp
-
-const Game = require("./server/Game/Game");
 
 //Builds all public files for pages
 app.use(express.static(path.join(__dirname+"/Main")));
 app.use(express.static(path.join(__dirname+"/Game")));
 
-//On every client connection
-io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
-  
-  // Listen to when a client creates a new room
-  socket.on("Create Game", ({name}) => {
-    const game = new Game(socket, name);
-    Game.addGame(game);
-  });
-  
-  // Listen to when a client wants to join a room
-  socket.on("Join Game", ({name, roomid}) => {
-    Game.joinGame(socket, name, roomid);
-  });
-  
-  // Listen to when the client wants to initiate a game
-  socket.on("Start Game", ({roomid}) => {
-    Game.startGame(socket, roomid);
-  });
+//Handles the io connection for game
+const ioFunc = require("./server/Game/io");
+ioFunc(http);
 
-  // Listen to when a client disconnect
-  socket.on("disconnect", () => {
-    // const game = Game.getGameBySocket(socket);
-    // game.disconnect(socket);
-    console.log("disconnecting:", socket.id);
-  });
-});
-
-//=========== routes ==============
 app.get("/game",(req, res) => {
-  res.sendFile(__dirname+"/Game/game.html");
+  res.sendFile(process.cwd()+"/Game/game.html");
 });
 
 // Handles page not found error
