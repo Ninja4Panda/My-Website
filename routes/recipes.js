7660f98cb9bf4e57233cf5d0b209a20f81a7261b
Ninja4Panda <ninja4panda@gmail.com>
2021-01-28 18:27:23 +1100

    working on frontend layout

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=> {
-    res.render('./src/Recipe/search.ejs', {});
+router.get('/', (req, res)=> { 
+    const recipes = [{
+        title: 'YOUR mum'
+    }, {
+        title: 'my mum'
+    }]
+    res.render('./src/Recipe/search.ejs', { recipes: recipes});
 });
 
 module.exports = router
