7ea9defd3e22e88ffd3755b6a864ffa778d0bf8f
Ninja4Panda <ninja4panda@gmail.com>
2021-02-04 14:47:04 +1100

    added photos

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,9 +4,19 @@ const Recipe = require('../src/Recipe/Model/recipe')
 
 router.get('/', async (req, res)=> { 
     try {
+        const pigs = []
         const recipes = await Recipe.find()
-        console.log(recipes)
-        res.render('./src/Recipe/views/search.ejs', { recipes: recipes})
+        recipes.forEach(recipe => {
+            switch(recipe.type) {
+                case "pig":
+                    pigs.push(recipe)
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        res.render('./src/Recipe/views/search.ejs', { pigs: pigs})
     } catch(err) {
         res.render('./src/Recipe/views/error.ejs')
     }
