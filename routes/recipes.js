6f6c1dbc98fdb82d9d920769161d64ab0570da7b
Ninja4Panda <ninja4panda@gmail.com>
2023-05-16 20:25:28 +1000

    test

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,15 +2,15 @@ const express = require('express')
 const router = express.Router()
 const Recipe = require('../src/Recipe/Model/recipe')
 const viewDir = './src/Recipe/views/'
-
-router.get('/', async (req, res)=> { 
+// test
+router.get('/', async (req, res) => {
     try {
         const pigs = []
         const others = []
         const recipes = await Recipe.find()
 
         recipes.forEach(recipe => {
-            switch(recipe.type) {
+            switch (recipe.type) {
                 case "pig":
                     pigs.push(recipe)
                     break;
@@ -22,27 +22,27 @@ router.get('/', async (req, res)=> {
             }
         });
 
-        res.render(viewDir+'main.ejs', { pigs: pigs, others: others})
-    } catch(err) {
-        res.render(viewDir+'error.ejs')
+        res.render(viewDir + 'main.ejs', { pigs: pigs, others: others })
+    } catch (err) {
+        res.render(viewDir + 'error.ejs')
     }
 })
 
-router.get('/:title', async (req, res)=> {
+router.get('/:title', async (req, res) => {
     try {
-        const recipe = await Recipe.findOne({ title:req.params.title })
-        if(recipe==null) {
+        const recipe = await Recipe.findOne({ title: req.params.title })
+        if (recipe == null) {
             res.redirect("/recipe")
         } else {
-            res.render(viewDir+'detailView.ejs', { recipe: recipe})
+            res.render(viewDir + 'detailView.ejs', { recipe: recipe })
         }
     } catch (err) {
-        res.render(viewDir+'error.ejs')
+        res.render(viewDir + 'error.ejs')
     }
 })
 
 // Handles url error
-router.use(function(e, req, res, next) {
+router.use(function (e, req, res, next) {
     res.redirect("/recipe")
 });
 
